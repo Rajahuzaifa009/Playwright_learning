f9b57c730a22c8abbdb0846b697a1faeeac16c59
Raja Huzaifa <huzaifaaqeel23@gmail.com>
2025-01-16 19:00:54 +0500

    newchangings

--- tests/demo.test.ts
+++ tests/demo.test.ts
@@ -53,3 +53,43 @@ test('login 2', async ({ page }) => {
   // // await page.close();
 
 
+  test('Adding data', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/OrangeHRM/);
+    await page.waitForLoadState('networkidle');
+    await page.getByPlaceholder("username").fill("admin");
+    await page.getByPlaceholder("Password").fill("admin123");
+    await page.locator('//input[@placeholder="Username"]').click();
+    await page.locator('//input[@placeholder="Password"]').click();  
+    await page.locator('//button[text()=" Login "]').click();
+    await page.waitForLoadState('networkidle');
+    expect(await page.locator('//h6[normalize-space()="Dashboard"]')).toBeVisible();
+    await page.locator('//span[normalize-space()="Admin"]').click();
+    expect(await page.locator('//h5[normalize-space()="System Users"]')).toBeVisible();
+    await page.waitForLoadState('networkidle');
+    await page.locator('//div[@class="--toggle"]//button[@type="button"]').click();
+    await page.waitForLoadState('networkidle');
+    await page.locator('//i[@class="oxd-icon bi-caret-down-fill"]').click();
+    await page.locator('//div[@class="oxd-input-group oxd-input-field-bottom-space"]//div//input[@class="oxd-input oxd-input--active"]').click();
+    await page.waitForLoadState('networkidle');
+    // expect(await page.locator('//label[normalize-space()="Username"]')).toBeVisible();
+    // await page.locator('//div[@class="oxd-input-group oxd-input-field-bottom-space"]//div//input[@class="oxd-input oxd-input--active"]').fill("maria");
+    await page.getByRole('textbox').nth(1).fill('maria');
+    await page.waitForLoadState('networkidle');
+    await page.locator('//button[normalize-space()="Search"]').click();
+    await page.waitForLoadState('networkidle');
+    expect(await page.getByText('(1) Record Found')).toBeVisible();
+    await page.locator('//i[@class="oxd-icon bi-pencil-fill"]').click();
+    expect(await page.locator('//h6[normalize-space()="Edit User"]')).toBeVisible();
+    await page.locator('//input[@placeholder="Type for hints..."]').click();
+    await page.getByRole('textbox').nth(1).fill('Maria Paula Saraiva Braga Albuquerque');
+    await page.locator('//button[normalize-space()="Save"]').click();
+
+
+
+
+  });
+
+
