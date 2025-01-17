c2c725c4d811a2cd20d33b724c4856a2cfdc5230
Raja Huzaifa <huzaifaaqeel23@gmail.com>
2025-01-17 19:28:33 +0500

    Job Section

--- tests/demo.test.ts
+++ tests/demo.test.ts
@@ -53,7 +53,7 @@ test('login 2', async ({ page }) => {
   // // await page.close();
 
 
-  test('Adding data', async ({ page }) => {
+  test('Adding data ', async ({ page }) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   
     // Expect a title "to contain" a substring.
@@ -79,24 +79,49 @@ test('login 2', async ({ page }) => {
     await page.getByRole('textbox').nth(1).fill('Nalim');
     await page.waitForLoadState('networkidle');
     await page.locator('//button[normalize-space()="Search"]').click();
-    await page.waitForLoadState('networkidle');
     await page.locator('//div[@class="orangehrm-paper-container"]//button[2]').click();
-    expect(await page.locator('//h6[normalize-space()="Edit User"]')).toBeVisible();
-    await page.locator('//button[normalize-space()="Save"]').click();
-
-
-
-
-
-
-    // expect(await page.locator('//div[@class="orangehrm-horizontal-padding orangehrm-vertical-padding"]]')).toBeVisible();
-    // expect(await page.getByText('(1) Record Found')).toBeVisible();
-    // await page.locator('//i[@class="oxd-icon bi-pencil-fill"]').click();
-    // expect(await page.locator('//h6[normalize-space()="Edit User"]')).toBeVisible();
-    // await page.locator('//input[@placeholder="Type for hints..."]').click();
-    // await page.getByRole('textbox').nth(1).fill('Maria Paula Saraiva Braga Albuquerque');
-    // await page.locator('//button[normalize-space()="Save"]').click();
+    await page.waitForLoadState('networkidle');
+    await expect(page.getByRole('heading', { name: 'Edit User' })).toBeVisible();
+    await (page.getByRole('button', { name: 'Save' })).click();
 
   });
 
 
+  test('Job Section ', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/OrangeHRM/);
+    await page.waitForLoadState('networkidle');
+    await page.getByPlaceholder("username").fill("admin");
+    await page.getByPlaceholder("Password").fill("admin123");
+    await page.locator('//input[@placeholder="Username"]').click();
+    await page.locator('//input[@placeholder="Password"]').click();  
+    await page.locator('//button[text()=" Login "]').click();
+    await page.waitForLoadState('networkidle');
+    expect(await page.locator('//h6[normalize-space()="Dashboard"]')).toBeVisible();
+    await page.locator('//span[normalize-space()="Admin"]').click();
+    expect(await page.locator('//h5[normalize-space()="System Users"]')).toBeVisible();
+    await page.waitForLoadState('networkidle');
+    await page.locator('//div[@class="--toggle"]//button[@type="button"]').click();
+    await page.waitForLoadState('networkidle');
+    await page.locator('//i[@class="oxd-icon bi-caret-down-fill"]').click();
+    await page.locator('//div[@class="oxd-input-group oxd-input-field-bottom-space"]//div//input[@class="oxd-input oxd-input--active"]').click();
+    await page.waitForLoadState('networkidle');
+    expect(await page.locator('//label[normalize-space()="Username"]')).toBeVisible();
+    // await page.locator('//div[@class="oxd-input-group oxd-input-field-bottom-space"]//div//input[@class="oxd-input oxd-input--active"]').fill("Nalim");
+    await page.getByRole('textbox').nth(1).fill('Nalim');
+    await page.waitForLoadState('networkidle');
+    await page.locator('//button[normalize-space()="Search"]').click();
+    await page.locator('//div[@class="orangehrm-paper-container"]//button[2]').click();
+    await page.waitForLoadState('networkidle');
+    await expect(page.getByRole('heading', { name: 'Edit User' })).toBeVisible();
+    await expect(page).toHaveURL(/saveSystemUser\/280/);
+    await page.waitForLoadState('networkidle');
+    await page.getByRole('button', { name: 'Save' }).click();
+    await page.waitForLoadState('networkidle');
+    await expect(page).toHaveURL(/admin\/viewSystemUsers/);
+    await page.waitForLoadState('networkidle');
+    await page.locator('//header[@class="oxd-topbar"]//li[2]').click();
+    
+  });
